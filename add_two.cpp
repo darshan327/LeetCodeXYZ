cfe83ffaafb4073f9750d7b30f990ef270d00898
Darshan G.V <darshangv36@gmail.com>
2026-05-07 22:36:13 +0530

    addition

---
+++ add_two.cpp
@@ -0,0 +1,11 @@
+    // Leetcode Number: 2235. Add Two Integers
+
+    // time complexity: O(1)
+    // space complexity: O(1)
+
+    class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1+num2;
+    }
+};
