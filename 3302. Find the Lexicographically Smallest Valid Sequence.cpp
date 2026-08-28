46455040dd1a1d4a5736e89d4a2323e83619e90f
Darshan G.V <darshangv36@gmail.com>
2026-08-28 23:16:07 +0530

    lexi

---
+++ 3302. Find the Lexicographically Smallest Valid Sequence.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+
+        int n = word1.size();
+        int m = word2.size();
+
+        vector<int> ans(m);
+
+        vector<int> last(m, -1);
+
+        int i = n - 1;
+        int j = m - 1;
+
+        while (i >= 0 && j >= 0) {
+
+            if (word1[i] == word2[j]) {
+                last[j] = i;
+                j--;
+            }
+
+            i--;
+        }
+
+        bool canSkip = true;
+
+        j = 0;
+
+        for (i = 0; i < n; i++) {
+
+            if (j == m)
+                break;
+
+            if (word1[i] == word2[j]) {
+
+                ans[j] = i;
+                j++;
+            }
+
+            else if (canSkip &&
+                     (j == m - 1 || i < last[j + 1])) {
+
+                canSkip = false;
+
+                ans[j] = i;
+                j++;
+            }
+        }
+
+        if (j != m)
+            return {};
+
+        return ans;
+    }
+};
