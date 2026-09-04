ca445316b4f7f956a24aef287f0f13c2f044a63f
Darshan G.V <darshangv36@gmail.com>
2026-09-04 23:45:57 +0530

    smastin

---
+++ 3903. Smallest Stable Index I.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> prefix(n,0);
+        vector<int> suffix(n,0);
+        prefix[0]=nums[0];
+        suffix[n-1]=nums[n-1];
+        for(int i=1; i<n; i++){
+            prefix[i]=max(nums[i],prefix[i-1]);
+        }
+        for(int i=n-2; i>=0; i--){
+            suffix[i]=min(nums[i],suffix[i+1]);
+        }
+
+        for(int i=0; i<n; i++){
+            if(prefix[i]-suffix[i]<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
